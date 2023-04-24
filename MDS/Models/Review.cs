82025df43405d4139ca60464d334757e6ef8a0fd
adeladanescu12 <adela.danescu@yahoo.com>
2023-04-24 17:01:07 +0300

    modele

---
+++ MDS/Models/Review.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations;
+
+namespace MDS.Models
+{
+    public class Review
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Titlul review-ului  este obligatoriu")]
+        public string Titlu { get; set; }
+
+        [Required(ErrorMessage = "Continutul review-ului  este obligatoriu")]
+        public string Continut { get; set; }
+        public int? Rating { get; set; }
+
+        public int? HotelId { get; set; }
+        public string? UserId { get; set; }
+        public virtual ApplicationUser? User { get; set; }
+    }
+}
