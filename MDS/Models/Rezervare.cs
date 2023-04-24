c71ac69e9239a6e6af50a932b7f8d73692796c08
Teo1230 <teomihailescu@yahoo.com>
2023-04-24 17:01:40 +0300

    fgh

---
+++ MDS/Models/Rezervare.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MDS.Models
+{
+    public class Rezervare
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        public ICollection <String> ListaClienti { get; set; }
+        [Required(ErrorMessage = "Data de check in este obligatorie")]
+        public DateTime CheckIn { get; set; }
+
+        [Required(ErrorMessage = "Data de check out este obligatorie")]
+
+        public DateTime CheckOut { get; set; }
+
+        public float Suma { get; set; }
+
+        public virtual ApplicationUser? User { get; set; }
+
+        public String ? UserId { get; set; }
+        public int? CameraId { get; set;}
+
+        [NotMapped]
+        public IEnumerable<SelectListItem>? Camera { get; set; }
+
+    }
+}
