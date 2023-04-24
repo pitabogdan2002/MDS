b928ac4bcaac5aa873c925face0b4620c6693adb
pitabogdan2002 <bogdan-ioan.pita@s.unibuc.ro>
2023-04-24 17:01:28 +0300

    Creare camera + user

---
+++ MDS/Models/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Hosting;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MDS.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+        public virtual ICollection<Hotel>? ListaHoteluri { get; set; }
+
+        public virtual ICollection<Rezervare>? ListaRezervari { get; set; }
+
+        public virtual ICollection<Camera>? ListaCamere { get; set; }
+
+        public virtual ICollection<Review>? ListaReviews { get; set; }
+
+        public virtual ICollection<Tara>? ListaTari { get; set; }
+
+
+        public string? Nume{ get; set; }
+
+        public string? Prenume { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem>? AllRoles { get; set; }
+    }
+}
---
+++ MDS/Models/Camera.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace MDS.Models
+{
+    public class Camera
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Numele camerei este obligatoriu")]
+        public string Nume{ get; set; }
+        
+        [Required(ErrorMessage = "Capacitatea camerei este obligatorie")]
+        public int Capacitate { get; set; }
+
+        [Required(ErrorMessage = "Descrierea camerei este obligatorie")]
+        public string Descriere { get; set; }
+
+        [Required(ErrorMessage = "Pretul camerei este obligatoriu")]
+        public float PretNoapte{ get; set; }
+
+        public int HotelId { get; set; }
+
+        public string? UserId { get; set; }
+
+        public virtual ApplicationUser? User { get; set; }
+
+        public virtual Hotel? Hotel { get; set; }
+
+        public virtual ICollection<Rezervare> ListaRezervari { get; set; }
+
+        [NotMapped]
+        public IEnumerable<SelectListItem> Hotel { get; set; }
+
+        [NotMapped]
+        public bool Disponibila { get; set; }
+
+        
+    }
+}
