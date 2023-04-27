a98c8be27b79d3c03e13d2173b87857829b2ef0f
Teo1230 <teomihailescu@yahoo.com>
2023-04-27 20:58:58 +0300

    Adela

---
+++ MDS/Controllers/Reviewscontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDS.Controllers
+{
+    public class Reviewscontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MDS/Controllers/TariController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MDS.Controllers
+{
+    public class TariController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
